a4793281b9100fe09867fd15994ba311e5adf10f
Soniya Tiwari <113426704+Soniya-Tiwari@users.noreply.github.com>
2024-01-26 20:45:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Web Designer", "Problem Solving", "System Designing", "Freelancer"],
+        strings: ["Software Developer", "Web Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Web Designer", "Problem Solving", "System Designing", "Freelancer"],
+        strings: ["Software Developer", "Web Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
